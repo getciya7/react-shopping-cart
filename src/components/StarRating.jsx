a326571f5733c5d0fc74cde46cfb55ba705e314c
Getci123 <getciya7@gmail.com>
2024-06-30 18:28:00 +0530

    Star rating Component

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,23 @@
+// src/components/StarRating.jsx
+import React from "react";
+
+// Star rating Component
+const StarRating = ({ rating }) => {
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    stars.push(
+      <i
+        key={i}
+        className={`bi-star${i <= rating ? "-fill" : ""}`}
+        style={{ color: "#ffc107" }}
+      ></i>
+    );
+  }
+  return (
+    <div className="d-flex justify-content-center small text-warning mb-2">
+      {stars}
+    </div>
+  );
+};
+
+export default StarRating;
